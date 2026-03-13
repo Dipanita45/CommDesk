f3f48dcf80e6b948ffa2ae2182ec74fb6a73aa05
Abhishek Kumar <abhishek9661342993@gmail.com>
2026-03-13 18:45:42 +0530

    feat: refactor MemberHeader component; replace Link with Button for improved navigation and update styling for better layout

--- src/features/Member/v1/Components/MemberHeader.tsx
+++ src/features/Member/v1/Components/MemberHeader.tsx
@@ -1,23 +1,25 @@
+import Button from "@/Component/ui/Button";
 import { IoPersonAdd } from "react-icons/io5";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 const MemberHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full py-[3vh] bg-white border-b-[1px] border-gray-300 flex   text-xl font-bold  justify-between ">
-      <div className="w-1/3 h-full  flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-gray-800  ml-5 mt-2">Member Managements</h1>
-        <span className="text-sm text-gray-500 ml-5 bg-gray-200 rounded-2xl py-1 px-3">
+      <div className="w-1/2 h-full  flex flex-col justify-between gap-2">
+        <h1 className="text-lg sm:text-[2.5vw] lg:text-2xl  font-bold text-gray-800  ml-5 mt-2">Member Managements</h1>
+        <span className="text-sm  lg:text-md  w-fit text-gray-600 ml-5 bg-gray-200 rounded-2xl py-1 px-3">
           1,248 Members
         </span>
       </div>
       <div className="w-1/3 h-full flex justify-end items-center gap-2 px-5">
-        <Link
-          to="/add-member"
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors flex gap-2 duration-300 mr-4 items-center"
-        >
-          {" "}
-          <IoPersonAdd /> Add Member
-        </Link>
+      <Button
+          text="Add Member"
+          onClick={() => navigate("/add-member")}
+          backgroundColor="#4f46e4"
+          icon={<IoPersonAdd />}
+        />
 
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsPJ9cm0-r5p50py0yUzvM5ZtEB-xWoJRPRA&s"
